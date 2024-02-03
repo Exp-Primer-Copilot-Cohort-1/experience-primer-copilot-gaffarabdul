9ba0aac03faa2bcadc08ffe760175e2670e1bad1
Gaffar <gaffar454@gmail.com>
2024-02-03 15:16:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,80 +1,17 @@
-// create web server
+// Create web server
+// Create a route
+// Create a route handler
+// Respond to the route handler with the route handler
+// Listen on a port
+
 const express = require('express');
+const commentsRouter = require('./routes/comments');
+
 const app = express();
+
 app.use(express.json());
-// create comments array
-const comments = [];
+app.use('/comments', commentsRouter);
 
-// create a route for comments
-app.get('/comments', (req, res) => {
-    res.send(comments);
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
 });
-
-// create a route for comments
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comments.push(comment);
-    res.send(comment);
-});
-
-// create a route for comments
-app.get('/comments/:id', (req, res) => {
-    const comment = comments.find(c => c.id === parseInt(req.params.id));
-    if (!comment) res.status(404).send('The comment with the given ID was not found');
-    res.send(comment);
-});
-
-// create a route for comments
-app.listen(3000, () => console.log('Listening on port 3000...'));
-
-// Path: index.js
-// create a route for comments
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-const comments = [];
-
-app.get('/comments', (req, res) => {
-    res.send(comments);
-});
-
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comments.push(comment);
-    res.send(comment);
-});
-
-app.get('/comments/:id', (req, res) => {
-    const comment = comments.find(c => c.id === parseInt(req.params.id));
-    if (!comment) res.status(404).send('The comment with the given ID was not found');
-    res.send(comment);
-});
-
-app.listen(3000, () => console.log('Listening on port 3000...'));
-
-// Path: comments.js
-// create a route for comments
-const express = require('express');
-const app = express();
-app.use(express.json());
-
-const comments = [];
-
-app.get('/comments', (req, res) => {
-    res.send(comments);
-});
-
-app.post('/comments', (req, res) => {
-    const comment = req.body;
-    comments.push(comment);
-    res.send(comment);
-});
-
-app.get('/comments/:id', (req, res) => {
-    const comment = comments.find(c => c.id === parseInt(req.params.id));
-    if (!comment) res.status(404).send('The comment with the given ID was not found');
-    res.send(comment);
-});
-
-app.listen(3000, () => console.log('Listening on port 3000...
